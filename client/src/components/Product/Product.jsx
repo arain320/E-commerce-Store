a97da1e80ef081681a2ad5aa2d3fa85d090348a8
M Hasnain Saleem <husnainsaleem3512@gmail.com>
2023-07-17 11:39:25 +0500

    code push to github

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,201 +1,85 @@
-import React from 'react'
-import "./Product.scss"
-import {NavLink} from "react-router-dom"
+import React, { useState, useEffect } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Product = () => {
+  const navigate = useNavigate();
+  const [result, setResult] = useState([]);
+  //get product of special tag from backend
+  const getProduct = async () => {
+    const res = await axios.get("http://localhost:5000/api/special", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (res.data.status === 401 || !res.data) {
+      console.log("something went wrong");
+    } else {
+      setResult(res.data.getItems);
+    }
+  };
+  useEffect(() => {
+    getProduct();
+  }, []);
   return (
     <>
-        <div className="product">
-            <div className="product-heading">
-                <span>Just for u</span>
-            </div>
-            <div className="product-container">
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger and other thingggggg which we need with mobile</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile2.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile2.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile2.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile2.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-                <NavLink to="/Products">
-                <div className="product-card">
-                    <div className="image">
-                        <img src="./images/mobile1.jpg" alt="" />
-                    </div>
-                    <div className="name">
-                        <span>samsung mobile with daba and charger</span>
-                    </div>
-                    <div className="price">
-                        <span>RS:100000</span>
-                        <span>120000</span>
-                    </div>
-                </div>
-                </NavLink>
-            </div>
+      <div className="product">
+        <div className="product-heading">
+          <span>Just for u</span>
+          <p>
+            <NavLink to="/products">
+              Shop more <i class="fa-sharp fa-solid fa-right-long"></i>{" "}
+            </NavLink>
+          </p>
         </div>
-     </>
-  )
-}
+        <div className="product-container">
+          {result.length > 0
+            ? result.map((product, index) => {
+                return (
+                  <>
+                    <div
+                      className="product-card"
+                      onClick={() => {
+                        navigate(`/detail/${product._id}`);
+                      }}
+                    >
+                      <div className="image">
+                        <img src={product.path} alt="product" />
+                      </div>
+                      <div className="name">
+                        <span>{product.name} </span>
+                      </div>
+                      <div className="price">
+                        <span>
+                          Rs:{" "}
+                          {new Intl.NumberFormat("en-IN", {}).format(
+                            product.price
+                          )}
+                        </span>
+                        <span>
+                          {new Intl.NumberFormat("en-IN", {}).format(
+                            product.lastprice
+                          )}
+                        </span>
+                      </div>
+                      <div className="star-sold">
+                        <div className="star">
+                          <i class="fa-solid fa-star"></i>{" "}
+                          <span>{product.star}/5</span>
+                        </div>
+                        <div className="sold">
+                          <span>( {product.sold} )</span>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                );
+              })
+            : ""}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Product
+export default Product;
